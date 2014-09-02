602828ded7d730a888daa372d2cff4d250a8d8ff
wynet321 <wynet321@163.com>
2014-09-02 16:01:20 +0800

    disable admin debug information.

--- YuShiWeb/admin/update.php
+++ YuShiWeb/admin/update.php
@@ -7,21 +7,21 @@ if (isset ( $_GET ['add'] )) {
 	for($i = 1; $i < 21; $i ++) {
 		rename ( "../" . $_REQUEST ["category"] . "/" . (20 - $i) . ".txt", "../" . $_REQUEST ["category"] . "/" . (21 - $i) . ".txt" );
 	}
-	file_put_contents ( "../".$_REQUEST ["category"]."/1.txt", "" );
+	file_put_contents ( "../" . $_REQUEST ["category"] . "/1.txt", "" );
 	
 	header ( 'Refresh: 0;URL=manage.html' );
 } else {
 	// upload
 	if ($_FILES ["file"] ["error"] > 0) {
-		echo "Error: " . $_FILES [$_REQUEST ["category"] ."file"] ["error"] . "<br>";
+		echo "Error: " . $_FILES [$_REQUEST ["category"] . "file"] ["error"] . "<br>";
 	} else {
-		echo "Upload: " . $_FILES [$_REQUEST ["category"] ."file"] ["name"] . "<br>";
-		echo "Type: " . $_FILES [$_REQUEST ["category"] ."file"] ["type"] . "<br>";
-		echo "Size: " . ($_FILES [$_REQUEST ["category"] ."file"] ["size"] / 1024) . " kB<br>";
-		echo "Stored in: " . $_FILES [$_REQUEST ["category"] ."file"] ["tmp_name"];
+		// echo "Upload: " . $_FILES [$_REQUEST ["category"] ."file"] ["name"] . "<br>";
+		// echo "Type: " . $_FILES [$_REQUEST ["category"] ."file"] ["type"] . "<br>";
+		// echo "Size: " . ($_FILES [$_REQUEST ["category"] ."file"] ["size"] / 1024) . " kB<br>";
+		// echo "Stored in: " . $_FILES [$_REQUEST ["category"] ."file"] ["tmp_name"];
 		$newsFileName = "../" . $_REQUEST ["category"] . "/" . $_POST ['list'] . ".txt";
-		if (move_uploaded_file ( $_FILES [$_REQUEST ["category"] ."file"] ["tmp_name"], $newsFileName ))
-header ( 'Refresh:0;URL=manage.html' );
+		if (move_uploaded_file ( $_FILES [$_REQUEST ["category"] . "file"] ["tmp_name"], $newsFileName ))
+			header ( 'Refresh:0;URL=manage.html' );
 		else
 			echo "Error: " . $newsFileName . " upload fail!";
 	}
