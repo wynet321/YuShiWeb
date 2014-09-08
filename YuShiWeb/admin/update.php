cae5d9418ccb14d53d4a0746f8bb6cd687ef6c50
wynet321 <wynet321@163.com>
2014-09-08 21:45:35 +0800

    fix bug on update.php 1. rename 0.txt(nonexist), remove one loop of rename. 2. if($_FILE) lost $_REQUEST part.

--- YuShiWeb/admin/update.php
+++ YuShiWeb/admin/update.php
@@ -4,21 +4,20 @@ if (isset ( $_GET ['add'] )) {
 	// add new
 	if (file_exists ( "../" . $_REQUEST ["category"] . "/20.txt" ))
 		unlink ( "../" . $_REQUEST ["category"] . "/20.txt" );
-	for($i = 1; $i < 21; $i ++) {
+	for($i = 1; $i < 20; $i ++) {
 		rename ( "../" . $_REQUEST ["category"] . "/" . (20 - $i) . ".txt", "../" . $_REQUEST ["category"] . "/" . (21 - $i) . ".txt" );
 	}
 	file_put_contents ( "../" . $_REQUEST ["category"] . "/1.txt", "" );
-	
 	header ( 'Refresh: 0;URL=manage.html' );
 } else {
 	// upload
-	if ($_FILES ["file"] ["error"] > 0) {
-		echo "Error: " . $_FILES [$_REQUEST ["category"] . "file"] ["error"] . "<br>";
+	if ($_FILES [$_REQUEST ["category"] . "file"] ["error"] > 0) {
+		echo "Error: " . ($_FILES [$_REQUEST ["category"] . "file"] ["error"]) . "<br>";
 	} else {
-		// echo "Upload: " . $_FILES [$_REQUEST ["category"] ."file"] ["name"] . "<br>";
-		// echo "Type: " . $_FILES [$_REQUEST ["category"] ."file"] ["type"] . "<br>";
-		// echo "Size: " . ($_FILES [$_REQUEST ["category"] ."file"] ["size"] / 1024) . " kB<br>";
-		// echo "Stored in: " . $_FILES [$_REQUEST ["category"] ."file"] ["tmp_name"];
+		// echo "Upload: " . $_FILES [$_REQUEST ["category"]."file"] ["name"] . "<br>";
+		// echo "Type: " . $_FILES [$_REQUEST ["category"]."file"] ["type"] . "<br>";
+		// echo "Size: " . ($_FILES [$_REQUEST ["category"]."file"] ["size"] / 1024) . " kB<br>";
+		// echo "Stored in: " . $_FILES [$_REQUEST ["category"]."file"] ["tmp_name"];
 		$newsFileName = "../" . $_REQUEST ["category"] . "/" . $_POST ['list'] . ".txt";
 		if (move_uploaded_file ( $_FILES [$_REQUEST ["category"] . "file"] ["tmp_name"], $newsFileName ))
 			header ( 'Refresh:0;URL=manage.html' );
